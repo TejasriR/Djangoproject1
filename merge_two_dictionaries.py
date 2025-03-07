2f5ae3ae9bb185636b4526d9265aab4b8c36e275
tejasri <teju.dataengineer@gmail.com>
2025-03-07 20:03:48 +0530

    mergeing two dictionarys

---
+++ merge_two_dictionaries.py
@@ -0,0 +1,8 @@
+#9. Write a Python code to merge two dictionaries
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 3, 'c': 4}
+#**dict1 and **dict2 unpack the key-value pairs from dict1 and dict2.
+#The {} braces create a new dictionary that includes all the key-value pairs from both dict1 and dict2.
+
+merged = {**dict1, **dict2}
+print(merged) #{'a': 1, 'b': 3, 'c': 4}
